3dad452821657938fd4ba1811615b3bac85cc865
Pierre-Jean Fichet <pierrejean.fichet@posteo.net>
2013-03-14 19:28:50 +0100

    ADD: hardcoded support for troff language. FIX: -l option force filter mode.

--- version.c
+++ version.c
@@ -5,10 +5,11 @@
 #else
 #define	USED
 #endif
-static const char rcsid[] USED = "$Id: version.c,v 0.3 2013/03/14 10:49:03 pj Exp pj $";
+static const char rcsid[] USED = "$Id: version.c,v 0.4 2013/03/14 18:02:21 pj Exp pj $";
 /* SLIST
- * grind.c,v 0.6 2013/03/14 17:51:25 pj Exp pj $
+ * grind.c,v 0.7 2013/03/14 18:19:48 pj Exp pj $
  * grindefs.c,v 0.3 2013/03/13 18:10:05 pj Exp pj $
  * grindefs.src,v 0.3 2013/03/14 17:57:39 pj Exp pj $
+ * grindroff.c,v 0.1 2013/03/14 18:27:04 pj Exp pj $
  * regexp.c,v 0.4 2013/03/14 10:44:00 pj Exp pj $
 */
