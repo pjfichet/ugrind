971fae4e03ae059e9d6a846b344be60367e48fdd
Pierre-Jean Fichet <pierrejean.fichet@posteo.net>
2013-03-14 19:02:21 +0100

    FIX: filter mode is now default. DEL: two column mode, font size, and index options are deleted, since it's the job of the macro to handle that. FIX: tabstop is 4 by default. FIX: grind now use a new set of macros. See the manual page for a detailed list of these macros. DEL: hardcoded typographic tricks are deleted. ADD: the format of the grindefs file is now documented, in comments of the file. ADD: make, postxml, xml, refer and idx languages are added to grindefs file.

--- version.c
+++ version.c
@@ -5,10 +5,10 @@
 #else
 #define	USED
 #endif
-static const char rcsid[] USED = "$Id: version.c,v 0.2 2013/03/13 17:56:29 pj Exp pj $";
+static const char rcsid[] USED = "$Id: version.c,v 0.3 2013/03/14 10:49:03 pj Exp pj $";
 /* SLIST
- * grind.c,v 0.4 2013/03/14 10:33:07 pj Exp pj $
+ * grind.c,v 0.6 2013/03/14 17:51:25 pj Exp pj $
  * grindefs.c,v 0.3 2013/03/13 18:10:05 pj Exp pj $
- * grindefs.src,v 0.2 2013/03/13 17:52:59 pj Exp pj $
+ * grindefs.src,v 0.3 2013/03/14 17:57:39 pj Exp pj $
  * regexp.c,v 0.4 2013/03/14 10:44:00 pj Exp pj $
 */
