8b5a614d717262c1780c2040ce35b910ab3ca036
Pierre-Jean Fichet <pierrejean.fichet@posteo.net>
2013-03-15 11:55:21 +0100

    ADD: makefile.

--- version.c
+++ version.c
@@ -5,11 +5,12 @@
 #else
 #define	USED
 #endif
-static const char rcsid[] USED = "$Id: version.c,v 0.4 2013/03/14 18:02:21 pj Exp pj $";
+static const char rcsid[] USED = "$Id: version.c,v 0.5 2013/03/14 18:28:50 pj Exp pj $";
 /* SLIST
  * grind.c,v 0.7 2013/03/14 18:19:48 pj Exp pj $
  * grindefs.c,v 0.3 2013/03/13 18:10:05 pj Exp pj $
  * grindefs.src,v 0.3 2013/03/14 17:57:39 pj Exp pj $
  * grindroff.c,v 0.1 2013/03/14 18:27:04 pj Exp pj $
+ * makefile,v 0.1 2013/03/15 10:54:01 pj Exp pj $
  * regexp.c,v 0.4 2013/03/14 10:44:00 pj Exp pj $
 */
