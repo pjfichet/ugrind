90d7fe49de333eba6533c04eca805d5fb61b3e76
Pierre-Jean Fichet <pierrejean.fichet@posteo.net>
2013-03-24 18:29:19 +0100

    Use new name: Ugrind.

--- version.c
+++ version.c
@@ -5,12 +5,13 @@
 #else
 #define	USED
 #endif
-static const char rcsid[] USED = "$Id: version.c,v 0.5 2013/03/14 18:28:50 pj Exp pj $";
-/* SLIST
- * grind.c,v 0.7 2013/03/14 18:19:48 pj Exp pj $
- * grindefs.c,v 0.3 2013/03/13 18:10:05 pj Exp pj $
- * grindefs.src,v 0.3 2013/03/14 17:57:39 pj Exp pj $
- * grindroff.c,v 0.1 2013/03/14 18:27:04 pj Exp pj $
- * makefile,v 0.1 2013/03/15 10:54:01 pj Exp pj $
- * regexp.c,v 0.4 2013/03/14 10:44:00 pj Exp pj $
+static const char rcsid[] USED = "$Id: version.c,v 0.6 2013/03/15 10:55:21 pj Exp pj $";
+/* RCS ID LIST
+* makefile,v 0.3 2013/03/24 17:27:49 pj Exp
+* regexp.c,v 0.4 2013/03/14 10:44:00 pj Exp
+* ugrind.c,v 0.8 2013/03/24 11:01:13 pj Exp
+* ugrindefs.c,v 0.4 2013/03/24 11:03:25 pj Exp
+* ugrindefs.src,v 0.3 2013/03/14 17:57:39 pj Exp
+* ugrindroff.c,v 0.2 2013/03/24 11:04:59 pj Exp
+* VERSION,v 0.6 2013/03/22 11:09:37 pj Exp pj
 */
