7d23266b9df95ca63815bb949a665fe5bfc55b5b
Pierre-Jean Fichet <pierrejean.fichet@posteo.net>
2013-04-05 12:21:59 +0200

    Troff language: Bugfix, close properly "\\n" and "\\".

--- version.c
+++ version.c
@@ -5,13 +5,13 @@
 #else
 #define	USED
 #endif
-static const char rcsid[] USED = "$Id: version.c,v 0.6 2013/03/15 10:55:21 pj Exp pj $";
+static const char rcsid[] USED = "$Id: version.c,v 0.7 2013/03/24 17:29:19 pj Exp $";
 /* RCS ID LIST
 * makefile,v 0.3 2013/03/24 17:27:49 pj Exp
 * regexp.c,v 0.4 2013/03/14 10:44:00 pj Exp
 * ugrind.c,v 0.8 2013/03/24 11:01:13 pj Exp
 * ugrindefs.c,v 0.4 2013/03/24 11:03:25 pj Exp
 * ugrindefs.src,v 0.3 2013/03/14 17:57:39 pj Exp
-* ugrindroff.c,v 0.2 2013/03/24 11:04:59 pj Exp
-* VERSION,v 0.6 2013/03/22 11:09:37 pj Exp pj
+* ugrindroff.c,v 0.3 2013/04/04 19:34:54 pj Exp
+* VERSION,v 0.7 2013/03/24 17:29:19 pj Exp
 */
