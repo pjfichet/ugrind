ceeea30e28fbd7762009a06d189c47649ad0f515
Pierre-Jean Fichet <pierrejean.fichet@posteo.net>
2014-03-12 15:30:09 +0100

    version.c: troff files: fix link to license.

--- version.c
+++ version.c
@@ -5,7 +5,7 @@
 #else
 #define	USED
 #endif
-static const char rcsid[] USED = "$Id: version.c,v 0.13 2013/10/30 16:35:00 pj Exp pj $";
+static const char rcsid[] USED = "$Id: version.c,v 0.14 2013/10/30 20:29:24 pj Exp pj $";
 /* RCS ID LIST
 * makefile,v 0.7 2013/10/30 16:22:04 pj Exp pj
 * readme.tr,v 1.10 2013/10/30 20:29:01 pj Exp pj
@@ -15,6 +15,6 @@ static const char rcsid[] USED = "$Id: version.c,v 0.13 2013/10/30 16:35:00 pj E
 * ugrindefs.src,v 0.3 2013/03/14 17:57:39 pj Exp pj
 * ugrindroff.c,v 0.4 2013/04/09 16:55:53 pj Exp pj
 * ugrind.tr,v 1.13 2013/10/30 20:29:01 pj Exp pj
-* utroff-ugrind.tr,v 1.15 2013/09/08 15:43:13 pj Exp
-* VERSION,v 0.13 2013/10/30 16:35:09 pj Exp pj
+* utroff-ugrind.tr,v 1.15 2013/09/08 15:43:13 pj Exp pj
+* VERSION,v 0.14 2013/10/30 20:29:48 pj Exp pj
 */
